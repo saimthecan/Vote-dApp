e2af6d11b8131011ed27c01556eb6f9b71c0c577
saimthecan <sco11@protonmail.com>
2023-08-18 13:48:12 +0300

    update

--- src/components/fetchCandidates.js
+++ src/components/fetchCandidates.js
@@ -4,7 +4,7 @@ import contractAbi from "./contractAbi.json";
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/3a403683e05a4f589bf766c399c3c42b'));
 
-const contractAddress = '0xf98d5e0e0cc00a3f01b580834c4d36d780f1df5d';
+const contractAddress = '0xc31a47dd10c2d6b5700941d5f145ef0adc27dbd6';
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 
 export async function fetchCandidates() {
