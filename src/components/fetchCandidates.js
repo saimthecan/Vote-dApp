45a7533b683f83250939a38f00a2e9ef21dc842b
Saim Can Özgen <canozgen@protonmail.com>
2024-05-19 14:13:30 +0300

    Update fetchCandidates.js

--- src/components/fetchCandidates.js
+++ src/components/fetchCandidates.js
@@ -2,9 +2,9 @@ import contractAbi from "./contractAbi.json";
 
 
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/3a403683e05a4f589bf766c399c3c42b'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://sepolia.infura.io/v3/3a403683e05a4f589bf766c399c3c42b'));
 
-const contractAddress = '0xc31a47dd10c2d6b5700941d5f145ef0adc27dbd6';
+const contractAddress = '0xa24ac940da10d61d1aa4bdb667a8dbc779ba57b2';
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 
 export async function fetchCandidates() {
